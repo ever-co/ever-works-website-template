e601ffdf48a9172e313e36b70185e99581871b37
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2025-02-22 12:02:20 +0000

    feat: improve theme toggler by ensuring it mounts correctly

--- components/theme-toggler.tsx
+++ components/theme-toggler.tsx
@@ -3,9 +3,17 @@
 import { Button } from "@heroui/react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export function ThemeToggler() {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
     <div>
